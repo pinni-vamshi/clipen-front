fa110fe75ed019501e45bc5eb0da4755b0f9597c
pinni vamshi krishna <pinnivamshikrishna@gmail.com>
2026-05-15 22:12:37 +0530

    Initial Commit

---
+++ paste/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  paste
+//
+//  Created by vamshi krishna pinni on 15/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ paste/pasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  pasteApp.swift
+//  paste
+//
+//  Created by vamshi krishna pinni on 15/05/26.
+//
+
+import SwiftUI
+
+@main
+struct pasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
